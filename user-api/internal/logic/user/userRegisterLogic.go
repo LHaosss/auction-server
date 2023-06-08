33fab78392b51bfd385c0c660b8812c455f0ab52
lhaosss <1710713887@qq.com>
2023-06-08 17:08:50 +0800

    创建user-api基础框架

---
+++ user-api/internal/logic/user/userRegisterLogic.go
@@ -0,0 +1,30 @@
+package user
+
+import (
+	"context"
+
+	"auction_server/user-api/internal/svc"
+	"auction_server/user-api/internal/types"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type UserRegisterLogic struct {
+	logx.Logger
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+}
+
+func NewUserRegisterLogic(ctx context.Context, svcCtx *svc.ServiceContext) *UserRegisterLogic {
+	return &UserRegisterLogic{
+		Logger: logx.WithContext(ctx),
+		ctx:    ctx,
+		svcCtx: svcCtx,
+	}
+}
+
+func (l *UserRegisterLogic) UserRegister(req *types.UserRegisterReq) (resp *types.UserRegisterResp, err error) {
+	// todo: add your logic here and delete this line
+
+	return
+}
